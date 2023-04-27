92a413d1c196f7586af59c2f18fff84303993634
jumpstartcoding <116022896+jumpstartcoding@users.noreply.github.com>
2023-04-27 15:58:20 -0400

    new

--- WeatherApi.py
+++ WeatherApi.py
@@ -1,23 +1,31 @@
-#Author(s): Jevaughn Stewart
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[101]:
+
 
 #libraries 
 import numpy as np       
 import pandas as pd
+import re
 #functions
 def combineDT(ltemp, ldate):            #combine hour with tempature 
     finalL =[]
     for x in range(23):
         finalL.append((ltemp[x],ldate[x]))
     return finalL
-
 def anon(lis,lisTwo):                  # remove random stuff 
     finalL =[]
     finalTwoL=[]
+    lisA=[] 
     for x in lis:
         finalL.append(x.replace("]",""))
     for x in lisTwo:
         finalTwoL.append(x.replace("]}}",""))
-    return finalL,finalTwoL
+    for x in finalTwoL:
+        lisA.append(re.sub('(\d+.)(\d+).','\\1\\2',x))  # get rid of extra decimal
+    
+    return finalL, lisA
 
 # call api
 api_key=pd.read_csv('https://api.open-meteo.com/v1/forecast?latitude=18.00&longitude=-76.79&hourly=temperature_2m&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timeformat=iso8601&forecast_days=1&timezone=auto')
@@ -35,8 +43,17 @@ for elt in date:
 #get hrs    
 hrs= timeL[1:]
 
+
+
 hrs,tempF=anon(hrs,tempF)
 complete= combineDT (hrs,tempF)
 
 # (hrs,temp)
 complete
+
+
+# In[ ]:
+
+
+
+
